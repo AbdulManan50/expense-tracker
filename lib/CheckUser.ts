190b6d7ccd54f058c9f4f994f7038ccc0854ba11
Abdul Manan <abdul.manan@novatoresols.com>
2025-08-13 12:08:53 +0500

    authantication

---
+++ lib/CheckUser.ts
@@ -0,0 +1,30 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { db } from "./db";
+
+export const checkUser = async () => {
+    const user = await currentUser
+
+    if (!user) {
+        return null
+    }
+    const loggedInUser = await db.user.findunique({
+        where: {
+            clerkUserId: user.bind,
+        },
+    })
+    if (loggedInUser) {
+        return loggedInUser
+    }
+    const newUser = await db.user.create({
+        data: {
+            clerkUserId: user.id,
+            name: `${user.firstName} ${user.lastName}`,
+            imageUrl: user.imageUrl,
+            email: user.emailAddresses[0]?.emailAddress,
+
+        }
+    })
+
+    return newUser;
+
+}
